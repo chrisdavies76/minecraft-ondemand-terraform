413f9ca057a21532fb1b42e61845921e8aa08c7b
Miguel Almeida <migaxmoitax@gmail.com>
2021-10-21 19:58:07 +0100

    Go through all the log messages
    And handle empty lists of services, which happens if a cluster has been
    decommissioned.

--- base/lambda/lambda_function.py
+++ base/lambda/lambda_function.py
@@ -3,20 +3,32 @@ import boto3
 import base64
 import gzip
 
+ecs = boto3.client('ecs', region_name='${aws_region}')
+
+
 def lambda_handler(event, context):
-  log_message = json.loads(
+  log_events = json.loads(
     gzip.decompress(
       base64.b64decode(
         event['awslogs']['data']
       )
     ).decode('utf8')
-  )['logEvents'][0]['message']
+  )['logEvents']
 
-  host = log_message.split(' ')[3]
+  names = set([
+    extract_name(event['message']) for event in log_events
+  ])
 
-  name = host.split('.')[0]
+  for name in names:
+    handle_name(name)
 
-  ecs = boto3.client('ecs', region_name='${aws_region}')
+
+def extract_name(message):
+  host = message.split(' ')[3]
+  return host.split('.')[0]
+
+
+def handle_name(name):
   try:
     response = ecs.describe_services(
       cluster=name,
@@ -26,6 +38,10 @@ def lambda_handler(event, context):
     )
   except:
     print(f"[{name}] Can't describe service")
+    return
+
+  if len(response["services"]) == 0:
+    print(f"[{name}] Can't find the service")
     return
 
   desired = response["services"][0]["desiredCount"]
