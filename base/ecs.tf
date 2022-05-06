dbae9951308d0d329977ae1ff91472a9182e3f23
Miguel Almeida <migaxmoitax@gmail.com>
2022-05-07 00:32:14 +0100

    Upgrade aws provider to 4.x

--- base/ecs.tf
+++ base/ecs.tf
@@ -1,9 +1,14 @@
 resource "aws_ecs_cluster" "minecraft_ondemand_cluster" {
   name               = var.cluster_name
-  capacity_providers = ["FARGATE_SPOT"]
 
   setting {
     name  = "containerInsights"
     value = "enabled"
   }
+}
+
+resource "aws_ecs_cluster_capacity_providers" "minecraft_ondemand_cluster" {
+  cluster_name = aws_ecs_cluster.minecraft_ondemand_cluster.name
+
+  capacity_providers = ["FARGATE_SPOT"]
 }
--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.0"
   required_providers {
     aws = {
-      version = "~> 3.0"
+      version = "~> 4.0"
     }
   }
 }
